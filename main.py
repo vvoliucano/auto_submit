e13ed9954a4dbbfc95aa7db2dc2a1ba40d9bd53a
liucan <liu_can@pku.edu.cn>
2022-07-15 17:10:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,29 @@ import warnings
 import sys
 import os
 import re
+import random
 warnings.filterwarnings('ignore')
 
+# reason = "其他必要事项", start = "燕园", end = "校外（社会面）", gate = "东南门", reason_detail = "科研", country = "中国", province = "北京市", city = "市辖区", district = "海淀区", track = "海淀路", jiedao = "海淀街道"
 
 def go(config):
     conf = ConfigParser()
     conf.read(config, encoding='utf8')
 
-    campus, reason, detail = dict(conf['common']).values()
-    destination, track = dict(conf['out']).values()
-    habitation, district, street = dict(conf['in']).values()
-    capture = conf.getboolean('capture', '是否需要备案历史截图')
-    path = conf['capture']['截图保存路径']
-    wechat = conf.getboolean('wechat', '是否需要微信通知')
+    reason, start, end, gate, reason_detail, country, province, city, district, jiedao, track, wechat = dict(conf['common']).values()
 
-    run(driver_pjs, argconf.ID, argconf.PASSWORD, campus, argconf.MAIL_ADDRESS, argconf.PHONE_NUMBER, reason, detail, destination, track,
-        habitation, district, street, capture, path, wechat, argconf.SENDKEY)
+    reason_detail = random.choice(reason_detail.split("，"))
+    print(reason_detail)
+
+    gate = random.choice(gate.split('，'))
+    print(gate)
+
+    track = random.choice(track.split('，'))
+
+    print(track)
+    print(reason, start, end, gate, reason_detail, country, province, city, district, jiedao, track, wechat)
+
+    run(driver_pjs, argconf.ID, argconf.PASSWORD, reason, start, end, gate, reason_detail, country, province, city, district, jiedao, track)
 
 
 if __name__ == '__main__':
@@ -30,9 +37,9 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--ID', type=str)
     parser.add_argument('--PASSWORD', type=str)
-    parser.add_argument('--MAIL_ADDRESS', type=str)
-    parser.add_argument('--PHONE_NUMBER', type=str)
-    parser.add_argument('--SENDKEY', type=str)
+    # parser.add_argument('--MAIL_ADDRESS', type=str)
+    # parser.add_argument('--PHONE_NUMBER', type=str)
+    # parser.add_argument('--SENDKEY', type=str)
     argconf = parser.parse_args()
 
     chrome_options = Options()
@@ -41,8 +48,15 @@ if __name__ == '__main__':
             options=chrome_options,
             executable_path='/usr/bin/chromedriver',
             service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+    # driver_pjs = webdriver.Chrome(
+    #         options=chrome_options,
+    #         executable_path='/usr/bin/chromedriver',
+    #         service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+    # driver_pjs = webdriver.Chrome()
+
     print('Driver Launched\n')
 
     go('config.ini')
 
     driver_pjs.quit()
+
